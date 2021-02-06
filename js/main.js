7755ad10ba84a94fd89b835ddbfb856d6edc4d8a
Alex Shibanov <79175825855@yandex.ru>
2021-02-06 15:32:05 +0300

    full function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-alert('Hello, World!');
-
-// Получение случайного целого числа в заданном интервале, включительно 
 //(https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE)
 
 // function getRandomIntInclusive (min, max) {
@@ -9,9 +6,14 @@ alert('Hello, World!');
 //   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 // }
 
-// Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна. Пример использования функции:
-// имя_функции(проверяемая_строка, максимальная длина); // Результат: true, если строка проходит по длине, и false — если не проходит
+// alert(getRandomIntInclusive( 100, 500 ));
 
 // function checkLength (stringCheck, maxLenght) {
-//  // что-то
+//   if (stringCheck.length <= maxLenght) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 // }
+
+// alert(checkLength( 'hello', 5 ));
