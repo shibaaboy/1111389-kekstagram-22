63dcb9cfba5b461433997a617128969c20d2c827
Alex Shibanov <79175825855@yandex.ru>
2021-02-06 20:38:24 +0300

    mistake

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-//(https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE)
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-// function getRandomIntInclusive (min, max) {
-//   min = Math.ceil(min); 
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-// }
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min); 
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
 
-// alert(getRandomIntInclusive( 100, 500 ));
+alert(getRandomIntInclusive( 100, 500 ));
 
-// function checkLength (stringCheck, maxLenght) {
-//   if (stringCheck.length <= maxLenght) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function checkLength (stringCheck, maxLenght) {
+  if (stringCheck.length <= maxLenght) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// alert(checkLength( 'hello', 5 ));
+alert(checkLength( 'hello', 5 ));
