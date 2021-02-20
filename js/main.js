d2ee3c7c22ea687bb710e925e0f13be7df265aa1
Alex Shibanov <79175825855@yandex.ru>
2021-02-20 23:49:33 +0300

    Финал объекта

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive (min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+function getRandomArrayInt(min, max) {
+  let numReserve = []
+  while (numReserve.length < 25) {
+    let randomNumber = getRandomIntInclusive(min, max);
+    let found = false;
+    for (let i = 0; i < numReserve.length; i++) {
+      if (numReserve[i] === randomNumber) {
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      numReserve[numReserve.length] = randomNumber;
+    }
+  }
+  return numReserve;
 }
 
 // alert(getRandomIntInclusive( 1, 25 ));
@@ -44,75 +62,33 @@ const NAMES = [
   'Анна',
 ];
 
-const createObj = () => {
-  const idPerson = getRandomIntInclusive(1,25);
-  const likesOfPhoto = getRandomIntInclusive(15,200);
-  const descriptionOfPhoto = getRandomIntInclusive(0,DESCRIPTIONS.length - 1);
-  const commentOfPhoto = getRandomIntInclusive(0,MESSAGES.length - 1);
-  const idOtherPerson = getRandomIntInclusive(1,1000);
-  const nameOfPerson = getRandomIntInclusive(0,NAMES.length -1);
+const createObj = (idPerson, urlPerson) => {
+  const likesOfPhoto = getRandomIntInclusive(15, 200);
+  const descriptionOfPhoto = getRandomIntInclusive(0, DESCRIPTIONS.length - 1);
+  const commentOfPhoto = getRandomIntInclusive(0, MESSAGES.length - 1);
+  const idOtherPerson = getRandomIntInclusive(1, 1000);
+  const nameOfPerson = getRandomIntInclusive(0, NAMES.length - 1);
+  const avatarOfPhoto = getRandomIntInclusive(1, 6);
 
   return {
-    id: idPerson, 
-    url: '', 
-    description: DESCRIPTIONS[descriptionOfPhoto], 
+    id: idPerson,
+    url: 'photos/'+ urlPerson + '.jpg',
+    description: DESCRIPTIONS[descriptionOfPhoto],
     likes: likesOfPhoto,
-    id1: idOtherPerson, 
-    avatar: '',
-    message: MESSAGES[commentOfPhoto],
-    name: nameOfPerson,
+    comments: [{
+      otherId: idOtherPerson,
+      avatar: 'img/avatar' + avatarOfPhoto + '.svg' ,
+      message: MESSAGES[commentOfPhoto],
+      name: NAMES[nameOfPerson],
+    }]
   }
 }
 
-alert(createObj());
+let idOfPerson = getRandomArrayInt(1, 25); 
+let urlPerson = getRandomArrayInt(1, 25); 
+let resultArr = []; 
+for (let i = 0; i < 25; i++) {
+  resultArr.push(createObj(idOfPerson[i],urlPerson[i]));
+}
 
-// const createObj = () => {
-//     const idPerson = getRandomIntInclusive(1,25);
-//     const likesOfPhoto = getRandomIntInclusive(15,200);
-//     const descriptionOfPhoto = getRandomIntInclusive(0,DESCRIPTIONS.length - 1);
-//     const commentOfPhoto = getRandomIntInclusive(0,MESSAGES.length - 1);
-//     const idOtherPerson = getRandomIntInclusive(1,1000);
-//     const nameOfPerson = getRandomIntInclusive(0,NAMES.length -1);
-
-//     return {
-//         id: idPerson, 
-//         url: '', 
-//         description: DESCRIPTIONS[descriptionOfPhoto], 
-//         likes: likesOfPhoto,
-//         comment: [ // РУГАЕТСЯ НА ТО, ЧТО ТАК НАПИСАЛ :(
-//             {
-//                 otherId: idOtherPerson, 
-//                 avatar: '',
-//                 message: MESSAGES[commentOfPhoto],
-//                 name: nameOfPerson,
-//             }
-//         ]
-//     }
-// }
-
-
-// Возможно этот код или нет (для id и картинок) :\ https://myrusakov.ru/js-random-numbers.html
-// var numPool = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
-
-// function shuffle(numPool) {
-//   for(var j, x, i = numPool.length; i; j = parseInt(Math.random() * i), x = numPool[--i], numPool[i] = numPool[j], numPool[j] = x);
-//   return numPool;
-// };
-// var randomResult = shuffle(numPool);
-// while( randomResult.length > 0 ) {
-//   console.log( randomResult.pop() );
-// }
-
-// Или это ...
-// var numReserve = []
-// while (numReserve.length < 12) {
-//   var randomNumber = Math.ceil(Math.random() * 1000);
-//   var found = false;
-//   for (var i = 0; i < numReserve.length; i++) {
-//   if (numReserve[i] === randomNumber){
-//    found = true;
-//    break;
-//   }
-//   }
-//   if (!found) { numReserve[numReserve.length]=randomNumber; }
-// }
+console.log(resultArr);
